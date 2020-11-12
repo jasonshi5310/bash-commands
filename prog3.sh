78b02bc9f7a37aa3d56f31973ccf9e293a2968d1
jasonshi5310 <jasonshi5310@gmail.com>
2020-11-12 02:07:30 -0500

    part 4 finished

--- prog3.sh
+++ prog3.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# echo $1
-# if [ ! $1 ]; then 
-#     echo "haha" 
-# fi
 if [ $# == 0 ]; then
     echo "missing data file"
     exit 0
@@ -17,21 +13,7 @@ length=$( echo ${arr[0]} | grep -o "," | wc -l )
 length=`expr $length + 0`
 lines=$( cat $1 | wc -l )
 lines=`expr $lines + 0`
-# echo "length $length"
-# echo "lines $lines"
 argv=("$@")
-# for (( i=0 ; i < $length ; i++ ))
-# do
-#     # echo ${argv[$i]}
-#     weight=1
-#     if [ ${argv[$i]} ]; then 
-#         weight=${argv[$i]}
-#     fi
-#     echo "weight: $weight"
-
-
-# done
-
 
 touch no_files_have_this_name_3
 cat $1 | sed 's/,/ /g' > no_files_have_this_name_3
@@ -53,18 +35,10 @@ do
         fi
         # echo "weight: $weight"
         weight_sum=`expr $weight_sum + $weight`
-        # for (( i=1; i < $lines; i++ ))
-        # do
         ith_row=(${arr[$i]})
-        # echo ${ith_row[@]}
-        # row_length=$( echo ${ith_row[@]} | grep -o ' ' | wc -l )
-        # row_length=`expr $row_length + 0`
-        # echo $row_length
         jth_column=${ith_row[$j]}
         jth_column=`expr $jth_column \* $weight`
         row_sum=`expr $row_sum + $jth_column`
-        # done
-        # echo "sum $sum"
     done
     # echo "row sum $row_sum"
     # echo $weight_sum
@@ -73,7 +47,5 @@ do
     sum=`expr $row_sum + $sum`
     # echo "sum $sum"
 done
-# echo $weight_sum
-# denominator=`expr $weight_sum \* \( $lines - 1 \)`
 sum=`expr $sum / \( $lines - 1 \)`
 echo $sum
--- prog4.sh
+++ prog4.sh
@@ -1,1 +1,37 @@
 #!/bin/bash
+if [ $# != 1 ]; then
+    echo "score directory missing"
+    exit 0
+fi
+if [ ! -d $1 ]; then
+    echo "$1 is not a directory"
+    exit 0
+fi
+
+for file in $1/*
+do
+    echo "$file:"
+    cat $file | sed 's/,/ /g' > no_files_have_this_name_4
+    readarray arr < no_files_have_this_name_4
+    rm no_files_have_this_name_4
+    points=(${arr[1]})
+    # echo $points
+    sum=0
+    for (( i=1 ; i <= 5 ; i++ ))
+    do 
+        temp=(${points[$i]})
+        # echo $temp
+        sum=`expr $sum + $temp`
+    done 
+    echo $sum
+    sum=`expr $sum \* 2`
+    if [ $sum -gt 92 ]; then
+        echo "A"
+    elif [ $sum -gt 79 ]; then
+        echo "B"
+    elif [ $sum -gt 65 ]; then
+        echo "C"
+    else
+        echo "D"
+    fi
+done
