d1d60fa3e2845a74ae08ccb3a31136caf0549d41
jasonshi5310 <jasonshi5310@gmail.com>
2020-11-16 19:27:57 -0500

    include A-Z and toupper

--- prog5.sh
+++ prog5.sh
@@ -12,17 +12,14 @@ if [ -d $2 ]; then
     exit 0
 fi
 
-cat $1| grep -o "\b[a-zA-Z][a-zA-Z][A-Za-z][A-Za-z]\b" > filename_part_5
+cat $1| grep -o "\b[A-Za-z][A-Za-z][A-Za-z][A-Za-z]\b" > filename_part_5
 readarray arr < filename_part_5
 rm filename_part_5
-# echo ${arr[@]}
-
+dict=$(awk '{print toupper($0)}' $2)
 for i in ${arr[@]}
 do 
-    found=( $(cat $2 | grep -o "$i" | wc -l))
-    # if [ $i == 'forc' ]; then
-    # echo $i
-    # fi
+    temp=$(echo "$i" | awk '{print toupper($0)}')
+    found=( $(grep -o "$temp" <<< $dict | wc -l))
     if [ $found == 0 ]; then
         echo $i
     fi
